bb5ea77c381c55ead89f78a025346f4e77b1c7ed
MD Sabbir Hossain <falaksabbir3@gmail.com>
2020-08-02 17:09:18 +0530

    new file is added

---
+++ script_for_real_problem/convert_images.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+def convert(file):
+  dest = "/opt/icons/" + file
+  src = os.getcwd() + "/images/" + file
+  im = Image.open(src)
+  im.rotate(90).resize((128, 128)).convert('RGB').save(dest, 'JPEG')
+
+if __name__ == "__main__":
+  file_list = os.listdir(os.getcwd() + "/images/")
+  for file in file_list:
+    convert(file)
